e60f1f9a4bf707b9c495b0ca449722d9592bed55
Dmitry Ponyatov <dponyatov@gmail.com>
2016-07-10 16:55:03 +0400

    + class:AST
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- py.py
+++ py.py
@@ -1,4 +1,16 @@
 import wx
 import ply
 
+class AST:
+    def __init__(self,V): self.tag=self.__class__.__name__ ; self.val=V
+    nest=[]
+    def push(self,o): self.nest.append(o) ; return self
+    def head(self): return "<%s:%s>"%(self.tag,self.val)
+    def dump(self,depth=0):
+        S="\n"+depth*"\t"+self.head()
+        for i in self.nest: S += i.dump(depth+1)
+        return S
+    def __repr__(self): return self.head()
+    def __add__(self,o): self.push(o) ; return self
 
+print ( AST(123) + AST('Z') ) #.dump()
