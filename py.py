b2701d2dfb793043f01f96950283481d9cc8ec06
Dmitry Ponyatov <dponyatov@gmail.com>
2016-07-10 17:40:59 +0400

    + num op
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- py.py
+++ py.py
@@ -4,17 +4,24 @@ import ply
 class AST:
     def __init__(self,V):
         self.tag=self.__class__.__name__ ; self.val=V ; self.nest=[]
-    def push(self,o): self.nest.append(o) ; return self
-    def head(self): return "<%s:%s>"%(self.tag,self.val)
+    def push(self,o):
+        self.nest.append(o) ; return self
+    def head(self):
+        return "<%s:%s>"%(self.tag,self.val)
     def dump(self,depth=0):
         S="\n"+"\t"*depth+self.head()
         for i in self.nest: S+=i.dump(depth+1)
         return S
-    def __repr__(self): return self.head()
-    def __add__(self,o): return self.push(o)
+    def __repr__(self):
+        return self.dump()
+    def __add__(self,o):
+        return OP("+").push(self).push(o)
+    def __mul__(self,o):
+        return OP("*").push(self).push(o)
 
-Z=AST(123)
-Z.push ( AST('Z') )
+class NUM(AST): pass
 
-print Z.dump()
+class OP(AST): pass
+
+print NUM(2) + AST(3) * NUM(4)
 
