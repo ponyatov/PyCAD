c7543c3ac80d83e0932e707981c4f1fd5f218f03
Dmitry Ponyatov <dponyatov@gmail.com>
2016-07-10 17:34:56 +0400

    * fixed nest (static) -> self.nest
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- py.py
+++ py.py
@@ -2,15 +2,19 @@ import wx
 import ply
 
 class AST:
-    def __init__(self,V): self.tag=self.__class__.__name__ ; self.val=V
-    nest=[]
+    def __init__(self,V):
+        self.tag=self.__class__.__name__ ; self.val=V ; self.nest=[]
     def push(self,o): self.nest.append(o) ; return self
     def head(self): return "<%s:%s>"%(self.tag,self.val)
     def dump(self,depth=0):
-        S="\n"+depth*"\t"+self.head()
-        for i in self.nest: S += i.dump(depth+1)
+        S="\n"+"\t"*depth+self.head()
+        for i in self.nest: S+=i.dump(depth+1)
         return S
     def __repr__(self): return self.head()
-    def __add__(self,o): self.push(o) ; return self
+    def __add__(self,o): return self.push(o)
 
-print ( AST(123) + AST('Z') ) #.dump()
+Z=AST(123)
+Z.push ( AST('Z') )
+
+print Z.dump()
+
